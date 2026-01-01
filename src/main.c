e8b2e3014b5757dd58c7f609f3d8d2921dd4006d
Arrachnyd <arrachnyd@gmail.com>
2026-01-02 06:10:43 +1100

    Added to Github!

--- src/main.c
+++ src/main.c
@@ -6,10 +6,10 @@ int sum(int a, int b) {
 
 int main () {
 	printf("Hello, World!");
-	printf("\nI changed the code!");
+	printf("\nI added my repository to Github!");
 	
-	int a = 3;
-	int b = 2;
+	int a = 23;
+	int b = 52;
 	
 	printf("\nThe sum of %d and %d is %d!", a, b, sum(a, b));
 	
