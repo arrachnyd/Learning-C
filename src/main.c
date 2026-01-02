52df4c7aaf53e3123fbb59474c9149e121dd9d6f
Arrachnyd <arrachnyd@gmail.com>
2026-01-02 12:27:46 +1100

    Changed PrintF

--- src/main.c
+++ src/main.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
-int sum(int a, int b) {
-	return a + b;
-}
-
 int main () {
 	printf("Hello, World!");
 	printf("\nI added my repository to Github!");
 	
-	int a = 23;
-	int b = 52;
-	
-	printf("\nThe sum of %d and %d is %d!", a, b, sum(a, b));
+	printf("\nI like donuts!");
 	
 	return 0;
 }
